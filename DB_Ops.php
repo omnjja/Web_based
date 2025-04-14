18b1aa548dd04e95d46f488ed8e4198afafac4fe
Alia-eid <eida5635@gmail.com>
2025-04-14 12:38:10 +0200

    update

--- DB_Ops.php
+++ DB_Ops.php
@@ -4,11 +4,12 @@ $username = 'Alia';
 $password = '1234'; 
 $dbname = 'user_registration';
 
-function dbConnect() {
-    global $host, $username, $password, $dbname;
-    $conn = new mysqli($host, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    return $conn;
-} ?>
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+ ?>
